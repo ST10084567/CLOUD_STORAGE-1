7f3075784857e47858608e562d0adb6aee70a67c
Amanda Sphesihle Hlekwayo <104764425+ST10084567@users.noreply.github.com>
2024-08-30 17:45:00 +0200

    Add project files.

---
+++ CLOUD_STORAGE 1/Controllers/JacketOwnerController.cs
@@ -0,0 +1,52 @@
+﻿using Cloud_Storage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cloud_Storage.Controllers
+{
+    public class JacketOwnerController : Controller
+    {
+        private readonly TableStorageService _tableStorageService;
+
+        public JacketOwnerController(TableStorageService tableStorageService)
+        {
+            _tableStorageService = tableStorageService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var jacketOwner = await _tableStorageService.GetAllJacketOwnersAsync();
+            return View(jacketOwner);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(JacketOwner jacketOwner)
+        {
+            jacketOwner.PartitionKey = "JacketOwnerPartition";
+            jacketOwner.RowKey = Guid.NewGuid().ToString();
+
+            await _tableStorageService.AddJacketOwnerAsync(jacketOwner);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(string partitionKey, string rowKey)
+        {
+            await _tableStorageService.DeleteJacketAsync(partitionKey, rowKey);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(string partitionKey, string rowKey)
+        {
+            var jacketOwner = await _tableStorageService.GetJacketOwnerAsync(partitionKey, rowKey);
+            if (jacketOwner == null)
+            {
+                return NotFound();
+            }
+            return View(jacketOwner);
+        }
+    }
+}
